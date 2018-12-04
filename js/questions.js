f237a2b56f5a62a7f36c8357a4ca434ab5a35041
Dominika Szukała <dominika.szukala@gmail.com>
2018-12-04 11:15:57 +0100

    Questions added

--- js/questions.js
+++ js/questions.js
@@ -1,47 +1,83 @@
 const questions = [
 	{
-		question: "",
+		question: "Smoking kills. If you're killed, you've lost a very important part of your life.",
 		answers: {
 			a: "",
-			b: "",
+			b: "Brooke Shields",
 			c: ""
 		},
-		correctAnswer: ""
+		correctAnswer: "b"
 	},
 	{
-		question: "",
+		question: "Roses are red, violets are blue, I'm schizophrenic, and so am I.",
 		answers: {
-			a: "",
+			a: "Oscar Levante",
 			b: "",
 			c: ""
 		},
-		correctAnswer: ""
+		correctAnswer: "a"
 	},
 	{
-		question: "",
+		question: "Life is hard. After all, it kills you.",
 		answers: {
-			a: "",
+			a: "Katherine Hepburn",
 			b: "",
 			c: ""
 		},
-		correctAnswer: ""
+		correctAnswer: "a"
 	},
 	{
-		question: "",
+		question: "I have an unfortunate personality.",
 		answers: {
 			a: "",
 			b: "",
-			c: ""
+			c: "Orson Welles"
 		},
-		correctAnswer: ""
+		correctAnswer: "c"
 	},
 	{
-		question: "",
+		question: "Better to live one year as a tiger, than a hundred as a sheep.",
 		answers: {
-			a: "",
-			b: "",
+			a: "Napoleon Bonaparte",
+			b: "Madonna",
+			c: "Confucius"
+		},
+		correctAnswer: "b"
+	},
+	{
+		question: "Butterflies are always following me, everywhere I go.",
+		answers: {
+			a: "Mariah Carey",
+			b: "Britney Spears",
 			c: ""
 		},
-		correctAnswer: ""
+		correctAnswer: "a"
+	},
+	{
+		question: "I have taken more out of alcohol then alcohol has taken out of me.",
+		answers: {
+			a: "Kanye West",
+			b: "Winston Churchill",
+			c: "Lady Gaga"
+		},
+		correctAnswer: "b"
+	},
+	{
+		question: "The reason we're successful, darling? My overall charisma, of course.",
+		answers: {
+			a: "Rihanna",
+			b: "Barack Obama",
+			c: "Freddie Mercury"
+		},
+		correctAnswer: "c"
+	},
+	{
+		question: "If I were two-faced, would I be wearing this one?",
+		answers: {
+			a: "Abraham Lincoln",
+			b: "Marylin Monroe",
+			c: "Queen Elisabeth II"
+		},
+		correctAnswer: "a"
 	}
 ];
