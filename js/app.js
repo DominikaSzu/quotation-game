322af5b7a3496d216f29b7db54c8f6b31f0deba2
Dominika Szukała <dominika.szukala@gmail.com>
2018-12-04 13:00:06 +0100

    comments removed

--- js/app.js
+++ js/app.js
@@ -86,32 +86,6 @@ const questions = [
 	}
 ];
 
-// (buildQuiz = () => {
-// 	const output = [];
-
-// 	questions.forEach(
-// 		(currentQuestion, questionNumber) => {
-// 			const answers = [];
-
-// 			for(letter in currentQuestion.answers) {
-// 				answers.push(
-// 					`<label>
-// 						<input type="radio" name="question${questionNumber}" value="${letter}">
-// 					${letter}:
-// 					${currentQuestion.answers[letter]}
-// 					</label>`
-// 				);
-
-// 			}
-// 			output.push(
-// 				`<div class="question">${currentQuestion.question}</div>
-// 				<div class="answers">${answers.join('')}</div>`
-// 			);
-// 		}
-// 	);
-// 	quiz.innerHTML = output.join('');
-// })();
-
 (buildQuiz = () => {
 	const output = [];
 
